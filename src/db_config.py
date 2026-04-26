dfd4da14b7407c51330435836dc38c808a93d56c
furqanx <alghifarifurqan03@gmail.com>
2026-04-26 07:10:16 +0700

    fix: fixing something

--- src/db_config.py
+++ src/db_config.py
@@ -1,18 +1,21 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import logging
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
 def get_engine():
-    user     = os.getenv('DB_USER',     'postgres')
+    user     = os.getenv('DB_USER',    'postgres')
     password = os.getenv('DB_PASSWORD', '')
-    host     = os.getenv('DB_HOST',     'localhost')
-    port     = os.getenv('DB_PORT',     '5432')
-    dbname   = os.getenv('DB_NAME',     'wellfarm')
-    sslmode  = os.getenv('DB_SSLMODE',  'prefer')
+    host     = os.getenv('DB_HOST',    'localhost')
+    port     = os.getenv('DB_PORT',    '5432')
+    dbname   = os.getenv('DB_NAME',    'wellfarm')
+    sslmode  = os.getenv('DB_SSLMODE', 'prefer')
 
     try:
         engine = create_engine(
